30f83c3b967ee46aa01d10075aa23268b1387127
caiolouro <caiolouro@gmail.com>
2020-06-24 23:09:00 -0300

    finished first tests using red green refactor and adding core project

--- DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using DeskBooker.Core.Domain;
 using Xunit;
 
 namespace DeskBooker.Core.Processor
 {
     public class DeskBookingRequestProcessorTests
     {
+        private readonly DeskBookingRequestProcessor _processor;
+
+        // Setup for all file tests
+        public DeskBookingRequestProcessorTests()
+        {
+            _processor = new DeskBookingRequestProcessor();
+        }
+
+
         [Fact]
-        public void ShouldReturnDeskBookingResponseWithRequestValues()
+        public void BookDeskResponseShouldHaveRequestValues()
         {
             // TDD arrange
             var request = new DeskBookingRequest
@@ -16,17 +26,26 @@ namespace DeskBooker.Core.Processor
                 Email = "caio@louro.com.br",
                 Date = new DateTime(2020, 06, 23)
             };
-            var processor = new DeskBookingRequestProcessor();
 
             // TDD act
-            var result = processor.BookDesk(request);
+            var response = _processor.BookDesk(request);
 
             // TDD assert
-            Assert.NotNull(result);
-            Assert.Equal(request.FirstName, result.FirstName);
-            Assert.Equal(request.LastName, result.LastName);
-            Assert.Equal(request.Email, result.Email);
-            Assert.Equal(request.Date, result.Date);
+            Assert.NotNull(response);
+            Assert.Equal(request.FirstName, response.FirstName);
+            Assert.Equal(request.LastName, response.LastName);
+            Assert.Equal(request.Email, response.Email);
+            Assert.Equal(request.Date, response.Date);
+        }
+
+        [Fact]
+        public void BookDeskShouldThrowExceptionForNullRequest()
+        {
+            // Checks if the right type of exception is thrown
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
+
+            // Checks if the exception has the right argument null exposed
+            Assert.Equal("request", exception.ParamName);
         }
     }
 }
