2d08d47fc5682e05d6323c6f77f8c0f7f1e9bd11
Caio Louro <caio.louro@vtex.com.br>
2020-07-06 21:18:19 -0300

    first ui logic unit test

--- DeskBooker.Web/Pages/BookDesk.cshtml.cs
+++ DeskBooker.Web/Pages/BookDesk.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,12 +7,19 @@ namespace DeskBooker.Web.Pages
 {
   public class BookDeskModel : PageModel
   {
+    private readonly IDeskBookingRequestProcessor _deskBookingRequestProcessor;
+
+    public BookDeskModel(IDeskBookingRequestProcessor deskBookingRequestProcessor)
+    {
+      _deskBookingRequestProcessor = deskBookingRequestProcessor;
+    }
+
     [BindProperty]
     public DeskBookingRequest DeskBookingRequest { get; set; }
 
     public void OnPost()
     {
-      
+      _deskBookingRequestProcessor.BookDesk(DeskBookingRequest);
     }
   }
 }
